481d35e79eeb0e13fa8b35c087c8fc56b347a814
Francisco <frmauro8@gmail.com>
2023-04-25 18:31:32 -0300

    coda  test

--- codaTest.py
+++ codaTest.py
@@ -1,7 +1,20 @@
-from codaio import Coda, Document
-# Initialize by providing a coda object directly
-coda = Coda('5335e58b-1d04-474a-ab86-df2ec848ae9c')
+import requests
 
-doc = Document('memex', coda=coda)
+headers = {'Authorization': 'Bearer 7a208aeb-ea98-414c-8e25-34d988f85892'}
+uri = 'https://coda.io/apis/v1/docs/pOe5ixBASv/'
+params = {
+  'isOwner': True,
+  'query': 'New',
+}
+res = requests.get(uri, headers=headers).json()
+
+print(res);
+#print(f'First doc is: {res["items"][0]["name"]}')
+
+
+# from codaio import Coda, Document
+# # Initialize by providing a coda object directly
+# coda = Coda('7a208aeb-ea98-414c-8e25-34d988f85892', 'https://coda.io/apis/v1/docs/pOe5ixBASv')
+# doc = Document('pOe5ixBASv', coda=coda)
 
 #doc.list_tables()
