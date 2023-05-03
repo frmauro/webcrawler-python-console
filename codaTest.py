056875d9fc4d0da821df12feb3b574fc1263a2c8
Francisco <frmauro8@gmail.com>
2023-05-03 14:53:06 -0300

    many tests

--- codaTest.py
+++ codaTest.py
@@ -1,15 +1,27 @@
+# a url do doc é: https://coda.io/d/memex_dpOe5ixBASv
 import requests
 
 headers = {'Authorization': 'Bearer 7a208aeb-ea98-414c-8e25-34d988f85892'}
-uri = 'https://coda.io/apis/v1/docs/pOe5ixBASv/'
+#uri = 'https://coda.io/apis/v1/docs/pOe5ixBASv/tables/grid-wWfa52dKst'
+uri = f'https://coda.io/apis/v1/docs/pOe5ixBASv/tables/grid-wWfa52dKst/rows'
+
 params = {
   'isOwner': True,
   'query': 'New',
 }
-res = requests.get(uri, headers=headers).json()
 
-print(res);
+req = requests.get(uri, headers=headers).json()
+
+req.raise_for_status() # Throw if there was an error.
+res = req.json()
+print(f'Matching rows: {len(res["items"])}')
+
+
+
+#print(res);
+#print(f'The name of the first table is {res["items"][0]["name"]}')
 #print(f'First doc is: {res["items"][0]["name"]}')
+print(f'Table {res["name"]} has {res["rowCount"]} rows')
 
 
 # from codaio import Coda, Document
