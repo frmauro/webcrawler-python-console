78e4375ad3c1bae0f14f771b33bc772bc5997290
Francisco <frmauro8@gmail.com>
2023-05-03 18:36:49 -0300

    create new file

---
+++ codaUpdateColumnTest.py
@@ -0,0 +1,22 @@
+import requests
+
+headers = {'Authorization': 'Bearer 7a208aeb-ea98-414c-8e25-34d988f85892'}
+uri = f'https://coda.io/apis/v1/docs/pOe5ixBASv/tables/grid-wWfa52dKst/rows/i-zUab2yBZcx/'
+
+payload = {
+  'row': {
+    'cells': [
+      {'column': 'c-ILOZNEGduy', 'value': 'https://odia.ig.com.br'},
+    ],
+  },
+}
+
+
+
+req = requests.put(uri, headers=headers, json=payload)
+req.raise_for_status() # Throw if there was an error.
+res = req.json()
+
+print(f'Updated row {res["id"]}')
+
+# => This table's columns: Task, Duration (hr), Duration (min)
